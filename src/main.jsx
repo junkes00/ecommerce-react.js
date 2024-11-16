331e989188034e6a76c0135c0140ed77b0ad757d
Nicolas Junkes <nicolas.junkes00@gmail.com>
2024-11-16 13:18:12 -0300

    feat: add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./pages/Error";
+import Root from "./routes/root";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/routes/root.jsx
@@ -0,0 +1,9 @@
+import Header from "../Components/Header";
+
+export default function Root() {
+  return (
+    <div style={{ marginLeft: "auto", marginRight: "auto" }}>
+      <Header />
+    </div>
+  );
+}
